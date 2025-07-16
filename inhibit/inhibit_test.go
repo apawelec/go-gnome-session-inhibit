a2c33c467fc4d3004dcc716ba6fbd0b73273188c
Adam Pawelec <1340207+apawelec@users.noreply.github.com>
2025-07-16 12:11:37 +0200

    Little fancier tests.

--- inhibit/inhibit_test.go
+++ inhibit/inhibit_test.go
@@ -5,19 +5,34 @@ import (
 	"math/rand"
 	"testing"
 
+	"github.com/godbus/dbus/v5"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_Inhibit_Acquire(t *testing.T) {
 	is := assert.New(t)
 
+	// given
+	appId := generateAppId()
+	flags := Idle | Logout
+
 	// when
-	i, err := Acquire(generateAppId(), "Library testing", Idle|Logout)
+	i, err := Acquire(appId, "Library testing", flags)
+	t.Cleanup(func() { i.Release() })
 
 	// then
 	if is.NoError(err) {
 		is.NotZero(i.cookie)
 		is.NotNil(i)
+	}
+
+	if inhibitors, err := readActiveInhibitors(); is.NoError(err) {
+		predicate := func(inhibitorPath string) (bool, error) {
+			return inhibitorMatches(inhibitorPath, appId, flags)
+		}
+		if match, err := some(inhibitors, predicate); is.NoError(err) {
+			is.True(match)
+		}
 	}
 }
 
@@ -25,7 +40,10 @@ func Test_Inhibit_Release(t *testing.T) {
 	is := assert.New(t)
 
 	// given
-	i, err := Acquire(generateAppId(), "Library testing", Idle|Logout)
+	appId := generateAppId()
+	flags := Idle | Logout
+
+	i, err := Acquire(appId, "Library testing", flags)
 	is.NoError(err)
 
 	// when
@@ -35,13 +53,25 @@ func Test_Inhibit_Release(t *testing.T) {
 	if is.NoError(err) {
 		is.Zero(i.cookie)
 	}
+
+	if inhibitors, err := readActiveInhibitors(); is.NoError(err) {
+		predicate := func(inhibitorPath string) (bool, error) {
+			return inhibitorMatches(inhibitorPath, appId, flags)
+		}
+		if match, err := some(inhibitors, predicate); is.NoError(err) {
+			is.False(match)
+		}
+	}
 }
 
 func Test_No_Error_For_Multiple_Release(t *testing.T) {
 	is := assert.New(t)
 
 	// given
-	i, err := Acquire(generateAppId(), "Library testing", Idle|Logout)
+	appId := generateAppId()
+	flags := Idle | Logout
+
+	i, err := Acquire(appId, "Library testing", flags)
 	is.NoError(err)
 
 	err = i.Release()
@@ -55,5 +85,47 @@ func Test_No_Error_For_Multiple_Release(t *testing.T) {
 }
 
 func generateAppId() string {
-	return fmt.Sprintf("test-app-%03d", rand.Int31n(1000))
+	return fmt.Sprintf("go-gnome-session-inhibit-test-%04d", rand.Int31n(10000))
+}
+
+func readActiveInhibitors() ([]string, error) {
+	bus, err := dbus.SessionBus()
+	if err != nil {
+		return nil, err
+	}
+	var inhibitors []string
+	err = bus.Object("org.gnome.SessionManager", "/org/gnome/SessionManager").
+		Call("org.gnome.SessionManager.GetInhibitors", 0).
+		Store(&inhibitors)
+	return inhibitors, err
+
+}
+
+func inhibitorMatches(inhibitorPath string, expectedAppId string, expectedFlags InhibitFlag) (bool, error) {
+	bus, err := dbus.SessionBus()
+	if err != nil {
+		return false, nil
+	}
+	object := bus.Object("org.gnome.SessionManager", dbus.ObjectPath(inhibitorPath))
+
+	var appId string
+	if err := object.Call("org.gnome.SessionManager.Inhibitor.GetAppId", 0).Store(&appId); err != nil {
+		return false, err
+	}
+	var flags uint32
+	if err := object.Call("org.gnome.SessionManager.Inhibitor.GetFlags", 0).Store(&flags); err != nil {
+		return false, err
+	}
+	return appId == expectedAppId && flags == uint32(expectedFlags), nil
+}
+
+func some[T any](seq []T, predicate func(T) (bool, error)) (bool, error) {
+	for _, el := range seq {
+		if match, err := predicate(el); err != nil {
+			return false, err
+		} else if match {
+			return true, nil
+		}
+	}
+	return false, nil
 }
